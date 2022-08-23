6dcb92ae2ac660127a2f8e37a64fe429e60abe8f
vishal <vishaltyagi.dev098@gmail.com>
2022-08-23 07:04:19 +0530

    sort zero and one

---
+++ Array/Questions/SortZeroOne.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+void SortZeroOne(int arr[],int n){
+  int start=0,end=n-1;
+  while(start<end){
+    while(arr[start]==0 && start<end){
+      start++;
+    }
+    while(arr[end]==1 && start<end){
+      end--;
+    }
+
+    if(start<end){
+      swap(arr[start],arr[end]);
+      start++;
+      end--;
+    }
+  }
+}
+
+void printarr(int arr[],int n){
+  for(int i=0;i<n;i++){
+    cout<<arr[i]<<" ";
+  }
+}
+int main(){
+  int arr[7]={0,1,1,0,0,1,0};
+  
+  SortZeroOne(arr,7);
+  printarr(arr,7);
+  return 0;
+}
