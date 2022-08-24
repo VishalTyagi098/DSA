7b793da30095983a656c3ffc1aef37403f72cd3d
vishal <vishaltyagi.dev098@gmail.com>
2022-08-24 14:53:31 +0530

    Total occurences

---
+++ Array/Searching Algorithms/Binary Search Questions/TotalOccurences.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+int firstOccurence(int arr[],int n, int key){
+  int start=0;
+  int end=n-1;
+  int mid= start + (end-start)/2;
+  int ans=-1;
+
+  while (start<=end)
+  {
+    if(arr[mid]==key){
+      ans=mid;
+      end=mid-1;
+    }
+    else if(arr[mid]<key){
+      start=mid+1;
+    }
+    else if(arr[mid]>key){
+      end=mid-1;
+    }
+    mid=start+ (end-start)/2;
+  }
+
+  return ans;
+}
+
+int lastOccurence(int arr[],int n, int key){
+  int start=0;
+  int end=n-1;
+  int mid= start + (end-start)/2;
+  int ans=-1;
+
+  while (start<=end)
+  {
+    if(arr[mid]==key){
+      ans=mid;
+      start=mid+1;
+    }
+    else if(arr[mid]<key){
+      start=mid+1;
+    }
+    else if(arr[mid]>key){
+      end=mid-1;
+    }
+    mid=start+ (end-start)/2;
+  }
+
+  return ans;
+  
+}
+
+int main(){
+  int arr[15]={1,2,3,4,4,4,4,4,4,4,4,4,4,4,5};
+  int firstOcc=firstOccurence(arr,15,4);
+  int lastOcc=lastOccurence(arr,15,4);
+
+  int totalOcc=(lastOcc-firstOcc)+1;
+
+  cout<<"Total number of occurences: "<<totalOcc;
+
+  return 0;
+}
