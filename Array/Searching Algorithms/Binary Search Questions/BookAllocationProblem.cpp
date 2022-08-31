01817b6d8233e5639a4ffee2877c4b1f3654b667
vishal <vishaltyagi.dev098@gmail.com>
2022-08-31 11:24:20 +0530

    Book allocation problem (isPossibleSoln function)

--- Array/Searching Algorithms/Binary Search Questions/BookAllocationProblem.cpp
+++ Array/Searching Algorithms/Binary Search Questions/BookAllocationProblem.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
 using namespace std;
 
+// Function to check if the pages and books can be allocated according to the condition of question.
+bool isPossibleSoln(int arr[],int n, int m, int mid){
+  int studentCount=1;
+  int pageSum=0;
+
+  for(int i=0; i<n; i++){
+
+    // It will check if sum of the pages alloted to one child is less then or equal to the mid value. first it will start from student 1. Then when the studentCount is incremented, it will check for pageSum for those students
+    if(pageSum+arr[i]<=mid){
+      pageSum+=arr[i];
+    }
+    else{
+      studentCount++;
+      if(studentCount>m || arr[i]>mid){
+        return false;
+      }
+      
+      // This will assign starting value of pageSum for other students
+      pageSum=arr[i];
+    }
+  }
+  return true;
+
+}
+
 
 // Binary Search function for reducing our search space (0 to sum of all elements in array)
 int binarySearch(int arr[], int n,int m){
@@ -12,12 +37,12 @@ int binarySearch(int arr[], int n,int m){
   int e=sum;
   int mid= s +(e-s)/2;
 
-  int ans=0;
+  int ans=-1;
 
   while(s<=e){
 
     // If possible soln is found, all the numbers greater than mid will also be possible soln. But we need to find the minimum solution, so we put e=mid-1
-    if(isPossibleSoln(int arr[],int n,int m, int mid)){
+    if(isPossibleSoln(arr,n,m,mid)){
       ans=mid;
       e=mid-1;
     }
@@ -34,6 +59,7 @@ int binarySearch(int arr[], int n,int m){
 }
 
 int main(){
-
+  int arr[4]={10,20,30,40};
+  cout<<binarySearch(arr,4,2);
   return 0;
 }
