0f1386d2029c805e0d1f9132aed20d0e461cfa90
vishal <vishaltyagi.dev098@gmail.com>
2022-09-13 22:17:19 +0530

    selection sort (revised)

--- Array/Sorting Algorithms/SelectionSort.cpp
+++ Array/Sorting Algorithms/SelectionSort.cpp
@@ -4,7 +4,6 @@ using namespace std;
 void selectionSort(int arr[],int n){
   for(int i=0;i<n-1;i++){
     int minIndex=i;
-
     for(int j=i+1;j<n;j++){
       if(arr[j]<arr[minIndex]){
         minIndex=j;
