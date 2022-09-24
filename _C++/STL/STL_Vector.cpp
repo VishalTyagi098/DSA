9a6b3c9cff73d6cd658cf05723e131e59da13b16
vishal <vishaltyagi.dev098@gmail.com>
2022-09-24 12:28:00 +0530

    vector at()/front()/back() func

--- _C++/STL/STL_Vector.cpp
+++ _C++/STL/STL_Vector.cpp
@@ -16,9 +16,10 @@ int main(){
   v.push_back(3);
   cout<<"Capacity: "<<v.capacity()<<endl; // Capacity is 4 because vector 'doubles' the size
 
-  
-  
-
+  // at(), front(), back() functions
+  cout<<"Element at index 2: "<<v.at(2)<<endl;
+  cout<<"First element: "<<v.front()<<endl;
+  cout<<"Last element: "<<v.back()<<endl;
 
   return 0;
 }
