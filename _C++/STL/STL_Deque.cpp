991aaa98171af74b07affc99b37415aadc082a40
vishal <vishaltyagi.dev098@gmail.com>
2022-09-24 12:44:27 +0530

    deque push/pop func

---
+++ _C++/STL/STL_Deque.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <deque>
+
+using namespace std;
+
+int main(){
+  deque<int> d;
+
+  // push_back()/push_front()/pop_back()/pop_front fucntions
+
+  d.push_back(1);
+  d.push_back(2);
+  d.push_front(3);
+  d.push_front(4);
+  for(int i=0;i<d.size();i++){
+    cout<<d[i]<<" ";
+  }
+  cout<<endl;
+
+  d.pop_back();
+  d.pop_front();
+  for(int i=0;i<d.size();i++){
+    cout<<d[i]<<" ";
+  }
+  cout<<endl;
+
+
+  return 0;
+}
