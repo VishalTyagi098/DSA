c29c1cd8d3832fb079b8df3fe2bc1d94fd741155
vishal <vishaltyagi.dev098@gmail.com>
2022-09-24 11:26:42 +0530

    small change

--- Array/Sorting Algorithms/InsertionSort.cpp
+++ Array/Sorting Algorithms/InsertionSort.cpp
@@ -19,7 +19,7 @@ void insertionSort(int arr[],int n){
 }
 
 int main(){
-  int arr[5]={5,4,3,2,1};
+  int arr[5]={4,5,3,2,1};
   insertionSort(arr,5);
   for(int i=0;i<5;i++){
     cout<<arr[i]<<" ";
