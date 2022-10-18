fe871e8d5419ce8e664a1eb982719733aa690ed9
vishal <vishaltyagi.dev098@gmail.com>
2022-10-18 18:44:13 +0530

    circular LL

--- college practice/circular LL/insertion.cpp
+++ college practice/circular LL/insertion.cpp
@@ -8,12 +8,35 @@ struct Node{
   Node* next;
 };
 
-void insertAtHead(struct Node* &head, int new_data){
+void insertAtHead(struct Node* &head,int new_data){
   struct Node* new_node=(struct Node*)malloc(sizeof(struct Node));
   new_node->data=new_data;
+  new_node->next=NULL;
+
+  struct Node* ptr=head;
+
+  while(ptr->next!=head){
+    ptr=ptr->next;
+  }
+
+  
+}
+
+void printLL(struct Node* &head){
+  struct Node* ptr=head;
+  cout<<ptr->data;
+  while(ptr->next!=head){
+    cout<<ptr->data<<" ";
+    ptr=ptr->next;
+  }
 }
  
 int main()
 {
+  struct Node* node1=(struct Node*)malloc(sizeof(struct Node));
+  node1->data=10;
+  node1->next=node1;
+
+  printLL(node1);
     return 0;
 }
