e6b72e5b4bf70aea2926709d9382b697fc5ccb3f
vishal <vishaltyagi.dev098@gmail.com>
2022-11-05 14:50:19 +0530

    creation of BT

---
+++ college practice/Trees/Binary Trees/creation.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+ 
+// Binary tree's Node structure
+struct Node{
+  int data;
+  struct Node* left;
+  struct Node* right;
+};
+
+// function to create a newNode
+struct Node* newNode(int newData){
+  struct Node* node=(struct Node*)malloc(sizeof(struct Node));
+  node->data=newData;
+  node->left=NULL;
+  node->right=NULL;
+  return (node);
+} 
+
+// Function to build the tree
+Node* buildTree(struct Node* root){
+  // create root node first
+  cout<<"Enter root node data: "<<endl;
+  int newData;
+  cin>>newData;
+  root=newNode(newData);
+
+  // to insert NULL
+  if(newData==-1){
+    return NULL;
+  }
+
+  // Recursive function to insert elements on left side of a node
+  cout<<"Enter data for inserting on left side of "<<newData<<endl;
+  root->left=buildTree(root->left);
+
+  // Recursive function to insert elements on right side of a node
+  cout<<"Enter data for inserting on right side of "<<newData<<endl;
+  root->right=buildTree(root->right);
+
+  return root;
+}
+
+int main()
+{
+  // initially root =null
+  struct Node* root=NULL;
+  root=buildTree(root);
+    return 0;
+}
