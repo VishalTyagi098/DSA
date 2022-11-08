2e63a5a0a846b3f8789a6b04f752b483e07d4e15
vishal <vishaltyagi.dev098@gmail.com>
2022-11-08 12:02:24 +0530

    insertion in BST

--- college practice/Trees/Binary Search Tree/Insertion.cpp
+++ college practice/Trees/Binary Search Tree/Insertion.cpp
@@ -15,10 +15,55 @@ Node* newNode(int newData){
   return node;
 }
 
+// insertion algorithm
+Node* insertInBST(struct Node* root,int newData){
+  if(root==NULL){
+    root=newNode(newData);
+    return root;
+  }
 
+  if(newData<root->data){
+    root->left=insertInBST(root->left,newData);
+  }
+  else{
+    root->right=insertInBST(root->right,newData);
+  }
+
+  return root;
+}
+
+// function to take input
+void inputData(struct Node* &root){
+  int data;
+  //to insert data once
+  cin>>data;
+  while(data!=-1){
+    root=insertInBST(root,data);
+    //to insert consecutive data
+    cin>>data;
+  }
+
+}
+
+// inOrder Traversal to print tree data
+void inOrderTraversal(struct Node* root){
+  if(root==NULL){
+    return;
+  }
+
+  inOrderTraversal(root->left);
+  cout<<root->data<<" ";
+  inOrderTraversal(root->right);
+}
 
  
 int main()
 {
-    return 0;
+  struct Node* root=NULL;
+
+  cout<<"insert data"<<endl;
+  inputData(root);
+
+  inOrderTraversal(root);
+  return 0;
 }
