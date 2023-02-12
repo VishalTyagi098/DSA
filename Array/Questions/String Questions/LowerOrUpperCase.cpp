3e9cb3518dc50b8c21c433fd0c5aaf22fa6a812e
vishal <vishaltyagi.dev098@gmail.com>
2023-02-12 12:35:19 +0530

    lower and upper case

---
+++ Array/Questions/String Questions/LowerOrUpperCase.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+ 
+char toLowerCase(char ch){
+  if(ch>='a'&&ch<='b'){
+    return ch;
+  }
+  else{
+    // ASCII value of Capital character subtracted by 'A' gives integer value. When we add 'a', it gives that same character in lower form
+    char temp= ch-'A' + 'a';
+    return temp;
+  }
+}
+char toUpperCase(char ch){
+  if(ch>='A'&&ch<='B'){
+    return ch;
+  }
+  else{
+    // ASCII value of small character subtracted by 'A' gives integer value. When we add 'a', it gives that same character in upper form
+    char temp= ch-'a' + 'A';
+    return temp;
+  }
+}
+ 
+int main()
+{
+  cout<<"Lower Character: "<<toLowerCase('B')<<endl;
+  cout<<"Upper Character: "<<toUpperCase('b');
+    return 0;
+}
